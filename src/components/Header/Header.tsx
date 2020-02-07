a9ac0fbe78044068b704a336965c73db08ab8664
Aden Herold <aden.herold1@gmail.com>
2020-02-08 10:04:20 +1100

    Added logo to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from "react";
 import styles from "./Header.module.css";
+import logo from "../../assets/img/logo-white.png";
 
 const Header: FC = () => (
   <>
-    <header className={styles.header}>Header content</header>
+    <header className={styles.header}>
+      <div className={styles.logoBox}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+      </div>
+    </header>
   </>
 );
 
