39e175169b614005da9611cf121e6ea79800280b
Aden Herold <aden.herold1@gmail.com>
2020-02-08 09:50:51 +1100

    Added Header component. Added custom styles to render clipped background image with a green gradient tint. Added global styles in index.css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "../Header";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import styles from "./Header.module.css";
+
+const Header: FC = () => (
+  <>
+    <header className={styles.header}>Header content</header>
+  </>
+);
+
+export default Header;
